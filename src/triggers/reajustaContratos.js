70e3bf7798137f80739ce0b0d4fe68f7ee83cc4e
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2025-02-27 13:57:53 -0300

    refactor: comenta função de reajuste de contratos e remove script de teste

--- src/triggers/reajustaContratos.js
+++ src/triggers/reajustaContratos.js
@@ -1,16 +1,16 @@
-import axios from "axios";
+// import axios from "axios";
 
-async function reajustaContratos() {
-  try {
-    console.log("Iniciando reajuste de contratos...");
-    // DEV
-    await axios.get("http://localhost:8080/api/reajusta-contratos");
-    // PRD
-    //await axios.get("http://20.186.19.140/api/reajusta-contratos");
-    console.log("Reajuste concluído.");
-  } catch (err) {
-    console.error(`Erro ao reajustar contratos: ${err.message}`);
-  }
-}
+// async function reajustaContratos() {
+//   try {
+//     console.log("Iniciando reajuste de contratos...");
+//     // DEV
+//     await axios.get("http://localhost:8080/api/reajusta-contratos");
+//     // PRD
+//     //await axios.get("http://20.186.19.140/api/reajusta-contratos");
+//     console.log("Reajuste concluído.");
+//   } catch (err) {
+//     console.error(`Erro ao reajustar contratos: ${err.message}`);
+//   }
+// }
 
-reajustaContratos().then(contratos => console.log(contratos));
+// reajustaContratos().then(contratos => console.log(contratos));
