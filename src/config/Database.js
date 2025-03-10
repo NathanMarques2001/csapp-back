0cc6498045db760044671f726d9f971495448d11
NathanMarques2001 <nathanmarques20@yahoo.com.br>
2025-03-09 22:24:26 -0300

    feat: ajusta configuração de timezone e atualiza lógica de busca de vencimentos para considerar o dia anterior

--- src/config/Database.js
+++ src/config/Database.js
@@ -4,6 +4,11 @@ module.exports = {
   username: "root",
   password: "admin",
   database: "sistema_gerenciamento_contratos",
+  timezone: '-03:00',
+  dialectOptions: {
+    useUTC: false,
+    timezone: 'America/Sao_Paulo',
+  },
   define: {
     timestamps: true,
     underscored: true,
--- src/controllers/VencimentoContratosController.js
+++ src/controllers/VencimentoContratosController.js
@@ -1,5 +1,6 @@
 const VencimentoContratos = require('../models/VencimentoContratos');
 const Contrato = require('../models/Contrato');
+const { Op } = require('sequelize');
 
 module.exports = {
   async getAll(req, res) {
@@ -13,11 +14,21 @@ module.exports = {
 
   async getToday(req, res) {
     try {
+      const today = new Date();
+      let startOfDay = new Date(Date.UTC(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate(), 0, 0, 0, 0));
+      let endOfDay = new Date(Date.UTC(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate(), 23, 59, 59, 999));
+
+      startOfDay.setDate(startOfDay.getDate() - 1);
+      endOfDay.setDate(endOfDay.getDate() - 1);
+
       const vencimentos = await VencimentoContratos.findAll({
         where: {
-          data_vencimento: new Date()
+          data_vencimento: {
+            [Op.between]: [startOfDay, endOfDay]
+          }
         }
       });
+
       res.status(200).json({ vencimentos });
     } catch (error) {
       res.status(500).json({ error: error.message });
