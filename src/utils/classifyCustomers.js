6a21d2a586f1fd344a2bfa5d7061ca74f8379080
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2025-05-16 17:10:56 -0300

    refactor: simplifica a lógica de classificação de clientes e melhora a legibilidade do código

--- src/utils/classifyCustomers.js
+++ src/utils/classifyCustomers.js
@@ -2,41 +2,42 @@ const Contrato = require('../models/Contrato');
 const Cliente = require('../models/Cliente');
 
 async function classifyCustomers() {
-  let tipo = "";
-  let clientesPorFaturamento = [];
-
   try {
     const clientes = await Cliente.findAll();
+    const clientesPorFaturamento = [];
 
-    // Calcular o faturamento total de cada cliente
-    for (let cliente of clientes) {
-      const contratos = await Contrato.findAll({ where: { id_cliente: cliente.id, status: 'ativo' } });
-      const faturamentoTotal = contratos.reduce((total, contrato) => total + valueWithTax(parseFloat(contrato.valor_mensal), parseFloat(contrato.indice_reajuste)), 0);
+    for (const cliente of clientes) {
+      const contratos = await Contrato.findAll({
+        where: { id_cliente: cliente.id, status: 'ativo' }
+      });
+
+      const faturamentoTotal = contratos.reduce((total, contrato) =>
+        total + valueWithTax(parseFloat(contrato.valor_mensal), parseFloat(contrato.indice_reajuste)), 0);
+
       clientesPorFaturamento.push({ cliente, faturamentoTotal });
     }
 
-    // Ordenar os clientes pelo faturamento total em ordem decrescente
+    // Ordenar os clientes pelo faturamento (maior primeiro)
     clientesPorFaturamento.sort((a, b) => b.faturamentoTotal - a.faturamentoTotal);
 
-    // Classificar os clientes
     for (let i = 0; i < clientesPorFaturamento.length; i++) {
       const { cliente, faturamentoTotal } = clientesPorFaturamento[i];
 
-      if (faturamentoTotal >= 3000) {
-        tipo = "a";
-      } else if (faturamentoTotal >= 1000) {
-        tipo = "b";
-      } else {
-        tipo = "c";
-      }
+      let tipo;
 
-      // Aplicar a classificação "top 30" após verificar o faturamento
       if (i < 30) {
-        tipo = "top 30";
+        tipo = 'top 30';
+      } else if (faturamentoTotal >= 3000) {
+        tipo = 'a';
+      } else if (faturamentoTotal >= 1000) {
+        tipo = 'b';
+      } else {
+        tipo = 'c';
       }
 
       await Cliente.update({ tipo }, { where: { id: cliente.id } });
-      console.log(`Cliente ${cliente.id} atualizado para a categoria ${tipo} com sucesso!`);
+
+      console.log(`Cliente ${cliente.id} classificado como "${tipo}"`);
     }
   } catch (error) {
     console.error('Erro ao classificar os clientes:', error);
