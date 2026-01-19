ff0ba6fea5122c10680c86c6763436c5b22e53ca
Nathan Marques <nathan.brandao@fundaffemg.com.br>
2026-01-19 13:22:44 -0300

    feat: Implement daily contract notification cron job and a new email service.

--- src/services/email.js
+++ src/services/email.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 class Email {
   // PRD
-  static baseUrl = "https://csapp.prolinx.com.br/email/email";
+  static baseUrl = "https://csapp.prolinx.com.br/email";
   // DEV
   // static baseUrl = "http://localhost:9090/email";
   constructor() {
